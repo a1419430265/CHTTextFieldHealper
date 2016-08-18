1cc4aa59d5a73dcd257406e9b832ade68f532409
陈辉009820 <1419430265@qq.com>
2016-08-18 16:59:49 +0800

    111

---
+++ UITextField+CHTHealper.h
@@ -0,0 +1,30 @@
+//
+//  UITextField+CHTPositionChange.h
+//  CHTTextFieldHealper
+//
+//  Created by risenb_mac on 16/8/17.
+//  Copyright © 2016年 risenb_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (CHTHealper)
+
+/**
+ *  是否支持视图上移
+ */
+@property (nonatomic, assign) BOOL canMove;
+/**
+ *  点击回收键盘、移动的视图，默认是当前控制器的view
+ */
+@property (nonatomic, strong) UIView *moveView;
+/**
+ *  textfield底部距离键盘顶部的距离
+ */
+@property (nonatomic, assign) CGFloat heightToKeyboard;
+
+@property (nonatomic, assign, readonly) CGFloat keyboardY;
+@property (nonatomic, assign, readonly) CGFloat initialY;
+@property (nonatomic, strong, readonly) UITapGestureRecognizer *tapGesture;
+
+@end
---
+++ UITextField+CHTHealper.m
@@ -0,0 +1,213 @@
+//
+//  UITextField+CHTPositionChange.m
+//  CHTTextFieldHealper
+//
+//  Created by risenb_mac on 16/8/17.
+//  Copyright © 2016年 risenb_mac. All rights reserved.
+//
+
+#import "UITextField+CHTHealper.h"
+#import <objc/runtime.h>
+
+static char canMoveKey;
+static char moveViewKey;
+static char heightToKeyboardKey;
+static char initialYKey;
+static char tapGestureKey;
+static char keyboardYKey;
+
+@implementation UITextField (CHTHealper)
+@dynamic canMove;
+@dynamic moveView;
+@dynamic heightToKeyboard;
+@dynamic initialY;
+@dynamic tapGesture;
+@dynamic keyboardY;
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        SEL systemSel = @selector(initWithFrame:);
+        SEL mySel = @selector(setupInitWithFrame:);
+        [self exchangeSystemSel:systemSel bySel:mySel];
+        
+        SEL systemSel2 = @selector(becomeFirstResponder);
+        SEL mySel2 = @selector(newBecomeFirstResponder);
+        [self exchangeSystemSel:systemSel2 bySel:mySel2];
+        
+        SEL systemSel3 = @selector(resignFirstResponder);
+        SEL mySel3 = @selector(newResignFirstResponder);
+        [self exchangeSystemSel:systemSel3 bySel:mySel3];
+        
+        SEL systemSel4 = @selector(initWithCoder:);
+        SEL mySel4 = @selector(setupInitWithCoder:);
+        [self exchangeSystemSel:systemSel4 bySel:mySel4];
+    });
+    [super load];
+}
+
+// 交换方法
++ (void)exchangeSystemSel:(SEL)systemSel bySel:(SEL)mySel {
+    Method systemMethod = class_getInstanceMethod([self class], systemSel);
+    Method myMethod = class_getInstanceMethod([self class], mySel);
+    //首先动态添加方法，实现是被交换的方法，返回值表示添加成功还是失败
+    BOOL isAdd = class_addMethod(self, systemSel, method_getImplementation(myMethod), method_getTypeEncoding(myMethod));
+    if (isAdd) {
+        //如果成功，说明类中不存在这个方法的实现
+        //将被交换方法的实现替换到这个并不存在的实现
+        class_replaceMethod(self, mySel, method_getImplementation(systemMethod), method_getTypeEncoding(systemMethod));
+    }else{
+        //否则，交换两个方法的实现
+        method_exchangeImplementations(systemMethod, myMethod);
+    }
+}
+
+- (instancetype)setupInitWithCoder:(NSCoder *)aDecoder {
+    [self setup];
+    return [self setupInitWithCoder:aDecoder];
+}
+
+- (instancetype)setupInitWithFrame:(CGRect)frame {
+    [self setup];
+    return [self setupInitWithFrame:frame];
+}
+
+- (void)setup {
+    self.heightToKeyboard = 10;
+    self.canMove = YES;
+    self.keyboardY = 0;
+    self.tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction)];
+}
+
+- (void)showAction:(NSNotification *)sender {
+    if (!self.canMove) {
+        return;
+    }
+    self.keyboardY = [sender.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].origin.y;
+    [self showKeyboard];
+}
+
+- (void)hideAction:(NSNotification *)sender {
+    if (!self.canMove || self.keyboardY == 0) {
+        return;
+    }
+    [self hideKeyBoard];
+}
+
+- (void)showKeyboard {
+    if (self.keyboardY == 0) {
+        return;
+    }
+    CGFloat fieldYInWindow = [self convertPoint:self.bounds.origin toView:[UIApplication sharedApplication].keyWindow].y;
+    CGFloat height = (fieldYInWindow + self.heightToKeyboard + self.frame.size.height) - self.keyboardY;
+    CGFloat moveHeight = height > 0 ? height : 0;
+    [UIView animateWithDuration:0.25 animations:^{
+        CGRect rect = self.moveView.frame;
+        self.initialY = rect.origin.y;
+        rect.origin.y -= moveHeight;
+        self.moveView.frame = rect;
+    }];
+}
+
+- (void)hideKeyBoard {
+    [UIView animateWithDuration:0.25 animations:^{
+        CGRect rect = self.moveView.frame;
+        rect.origin.y = self.initialY;
+        self.moveView.frame = rect;
+    }];
+}
+
+- (BOOL)newBecomeFirstResponder {
+    if (self.moveView == nil) {
+        self.moveView = [self viewController].view;
+    }
+    if (![self.moveView.gestureRecognizers containsObject:self.tapGesture]) {
+        [self.moveView addGestureRecognizer:self.tapGesture];
+    }
+    if ([self isFirstResponder] || !self.canMove) {
+        return [self newBecomeFirstResponder];
+    }
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showAction:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(hideAction:) name:UIKeyboardWillHideNotification object:nil];
+    return [self newBecomeFirstResponder];
+}
+
+- (BOOL)newResignFirstResponder {
+    if ([self.moveView.gestureRecognizers containsObject:self.tapGesture]) {
+        [self.moveView removeGestureRecognizer:self.tapGesture];
+    }
+    if (!self.canMove) {
+        return [self newResignFirstResponder];
+    }
+    BOOL result = [self newResignFirstResponder];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+    [self hideKeyBoard];
+    return result;
+}
+
+- (void)tapAction {
+    [[self viewController].view endEditing:YES];
+}
+
+- (UIViewController *)viewController {
+    UIView *next = self;
+    while (1) {
+        UIResponder *nextResponder = [next nextResponder];
+        if ([nextResponder isKindOfClass:[UIViewController class]]) {
+            return (UIViewController *)nextResponder;
+        }
+        next = next.superview;
+    }
+    return nil;
+}
+
+- (void)setCanMove:(BOOL)canMove {
+    objc_setAssociatedObject(self, &canMoveKey, @(canMove), OBJC_ASSOCIATION_ASSIGN);
+}
+
+- (BOOL)canMove {
+    return [objc_getAssociatedObject(self, &canMoveKey) boolValue];
+}
+
+- (void)setHeightToKeyboard:(CGFloat)heightToKeyboard {
+    objc_setAssociatedObject(self, &heightToKeyboardKey, @(heightToKeyboard), OBJC_ASSOCIATION_ASSIGN);
+}
+
+- (CGFloat)heightToKeyboard {
+    return [objc_getAssociatedObject(self, &heightToKeyboardKey) floatValue];
+}
+
+- (void)setMoveView:(UIView *)moveView {
+    objc_setAssociatedObject(self, &moveViewKey, moveView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (UIView *)moveView {
+    return objc_getAssociatedObject(self, &moveViewKey);
+}
+
+- (void)setInitialY:(CGFloat)initialY {
+    objc_setAssociatedObject(self, &initialYKey, @(initialY), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (CGFloat)initialY {
+    return [objc_getAssociatedObject(self, &initialYKey) floatValue];
+}
+
+- (void)setTapGesture:(UITapGestureRecognizer *)tapGesture {
+    objc_setAssociatedObject(self, &tapGestureKey, tapGesture, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (UITapGestureRecognizer *)tapGesture {
+    return objc_getAssociatedObject(self, &tapGestureKey);
+}
+
+- (void)setKeyboardY:(CGFloat)keyboardY {
+    objc_setAssociatedObject(self, &keyboardYKey, @(keyboardY), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (CGFloat)keyboardY {
+    return [objc_getAssociatedObject(self, &keyboardYKey) floatValue];
+}
+
+@end
